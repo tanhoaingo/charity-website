ebad09cec19d4370f91eb2daf638a0ebcceb5b39
tanhoaingo <71320093+tanhoaingo@users.noreply.github.com>
2023-01-31 22:04:50 +0700

    init backend

---
+++ charity-support/src/test/java/com/charity/charitysupport/CharitySupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charity.charitysupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharitySupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
